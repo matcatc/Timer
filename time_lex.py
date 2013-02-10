cda00fcaef7737f67edf00176c1d104551795cdf
Matthew Todd <matcatprg@yahoo.com>
2013-02-09 16:43:40 -0800

    Added license info.

--- time_lex.py
+++ time_lex.py
@@ -2,6 +2,23 @@
 '''
 Tokenizer for time duration.
 
+
+@license
+    This file is part of Timer.
+
+    Timer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Timer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Timer.  If not, see <http://www.gnu.org/licenses/>.
+
 @date Mar 23, 2012
 @author Matthew Todd
 '''
--- timer.py
+++ timer.py
@@ -8,6 +8,23 @@ At this time, there is little no support for playing sound files through
 pre-packaged python3 libraries. Thus I (and any users) would have to manually
 install the libraries to use them. Thus the use of subprocess instead.
 
+
+@license
+    This file is part of Timer.
+
+    Timer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Timer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Timer.  If not, see <http://www.gnu.org/licenses/>.
+
 @author Matthew Todd
 @date Sep 3 2011
 
